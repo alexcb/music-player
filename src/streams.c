61f63c7fa98e839f11d9f3a9218187869a9b9845
Alex Couture-Beil <alex@mofo.ca>
2017-04-27 17:36:11 -0700

    wip

--- src/streams.c
+++ src/streams.c
@@ -29,20 +29,13 @@ int parse_streams(const char *path, Stream **p)
 		LOG_DEBUG("line=s got line", line);
 		if( !line[0] )
 			continue;
-		LOG_DEBUG("h1");
 		entry = malloc(sizeof(Stream));
-		LOG_DEBUG("h2");
 		s = strstr( line, " " );
-		LOG_DEBUG("h3");
 		entry->name = sdsnewlen( line, s - line );
-		LOG_DEBUG("h4");
 		entry->url = sdsnew( s + 1 );
-		LOG_DEBUG("h5");
 
 		SGLIB_SORTED_LIST_ADD( Stream, *p, entry, STREAM_COMPARATOR, next_ptr );
-		LOG_DEBUG("h6");
 	}
-	LOG_DEBUG("h7");
 	fclose( fp );
 	return 0;
 }
