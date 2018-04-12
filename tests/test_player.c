d53e629ee4737df0f3e19f629d214e7de2a2ee88
Alex Couture-Beil <alex@mofo.ca>
2018-04-11 21:54:03 -0700

    wip

--- tests/test_player.c
+++ tests/test_player.c
@@ -234,41 +234,14 @@ unsigned int testPlayerLoop()
 
 static void* test_feeder(void *data)
 {
-	return NULL;
-}
-
-unsigned int testPlayerSkip()
-{
-	Player *player = setupTestPlayer();
-	Playlist *playlist = setupTestPlaylist();
-
-	last_track_seen = 1;
-	player->audio_consumer = test_player_consumer_skip;
-
-	expected_buffer_size = 1024*1024*1024;
-	expected_buffer_read = 0;
-	expected_buffer_len = 0;
-	expected_buffer = malloc(expected_buffer_size);
-
-	pthread_mutex_init( &player->the_lock, NULL );
-
-	TEST_ASSERT( !init_circular_buffer( &player->circular_buffer, 5001 ) );
-	TEST_ASSERT( !play_queue_init( &player->play_queue ) );
-
-	pthread_t audio_thread;
-	TEST_ASSERT( !pthread_create( &audio_thread, NULL, (void *) &player_audio_thread_run, (void *) player) );
-
-	pthread_t feeder_thread;
-	TEST_ASSERT( !pthread_create( &feeder_thread, NULL, &test_feeder, (void*) NULL ) );
-
-	int seed = 1;
+	PlaylistItem *root = (PlaylistItem *data);
 
 	PlayQueueItem *pqi;
 	char *p;
 	size_t buffer_free;
 	int chunk_size = 500;
 	unsigned char track_num = 1;
-	for( PlaylistItem *current_item = playlist->root; current_item; current_item = current_item->next, track_num++ ) {
+	for( PlaylistItem *current_item = root; current_item; current_item = current_item->next, track_num++ ) {
 		LOG_DEBUG("adding pqi");
 		usleep(1);
 		pthread_mutex_lock( &player->the_lock );
@@ -302,6 +275,36 @@ unsigned int testPlayerSkip()
 			buffer_mark_written( &player->circular_buffer, chunk_size );
 		}
 	}
+
+	return NULL;
+}
+
+unsigned int testPlayerSkip()
+{
+	Player *player = setupTestPlayer();
+	Playlist *playlist = setupTestPlaylist();
+
+	last_track_seen = 1;
+	player->audio_consumer = test_player_consumer_skip;
+
+	expected_buffer_size = 1024*1024*1024;
+	expected_buffer_read = 0;
+	expected_buffer_len = 0;
+	expected_buffer = malloc(expected_buffer_size);
+
+	pthread_mutex_init( &player->the_lock, NULL );
+
+	TEST_ASSERT( !init_circular_buffer( &player->circular_buffer, 5001 ) );
+	TEST_ASSERT( !play_queue_init( &player->play_queue ) );
+
+	pthread_t audio_thread;
+	TEST_ASSERT( !pthread_create( &audio_thread, NULL, (void *) &player_audio_thread_run, (void *) player) );
+
+	pthread_t feeder_thread;
+	TEST_ASSERT( !pthread_create( &feeder_thread, NULL, &test_feeder, (void*) playlist->root ) );
+
+	int seed = 1;
+
 
 	unsigned char wait_until_track = 2;
 	printf("here\n");
