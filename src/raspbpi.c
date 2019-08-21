baef269063f66b6a05e981e1dd54d394dec84cc0
Alex Couture-Beil <alex@mofo.ca>
2019-08-21 09:56:22 -0700

    fix

--- src/raspbpi.c
+++ src/raspbpi.c
@@ -1,14 +1,15 @@
 #ifdef USE_RASP_PI
 
-#include "raspbpi.h"
+#	include "raspbpi.h"
 
-#include "timing.h"
-#include "log.h"
+#	include "log.h"
+#	include "timing.h"
 
-#include <pthread.h>
-#include <wiringPi.h>
+#	include <pthread.h>
+#	include <wiringPi.h>
 
-struct gpio_switch {
+struct gpio_switch
+{
 	int gpio_pin;
 	int last_state;
 	int current_state;
@@ -16,50 +17,50 @@ struct gpio_switch {
 	int held;
 
 	// immediately called on down
-	void (*on_down)(Player* p);
+	void ( *on_down )( Player* p );
 
 	// immediately called on up
-	void (*on_up)(Player* p);
+	void ( *on_up )( Player* p );
 
 	// called if held down for 2 seconds
-	void (*on_hold)(Player* p);
+	void ( *on_hold )( Player* p );
 };
 
-void next_track(Player *player)
+void next_track( Player* player )
 {
 	int res;
 	res = player_change_next_track( player, TRACK_CHANGE_IMMEDIATE );
 	if( res != 0 ) {
-		LOG_ERROR("err=d failed to change to next track", res);
+		LOG_ERROR( "err=d failed to change to next track", res );
 	}
 }
 
-void next_album(Player *player)
+void next_album( Player* player )
 {
 	int res;
 	res = player_change_next_album( player, TRACK_CHANGE_IMMEDIATE );
 	if( res != 0 ) {
-		LOG_ERROR("err=d failed to change to next album", res);
+		LOG_ERROR( "err=d failed to change to next album", res );
 	}
 }
 
-void next_playlist(Player *player)
+void next_playlist( Player* player )
 {
 	player_change_next_playlist( player, TRACK_CHANGE_IMMEDIATE );
 }
 
-void do_play(Player *player)
+void do_play( Player* player )
 {
 	player_set_playing( player, 1 );
 }
 
-void do_pause(Player *player)
+void do_pause( Player* player )
 {
 	player_set_playing( player, 0 );
 }
 
 struct gpio_switch switches[] = {
-#ifdef KITCHEN
+#	ifdef KITCHEN
 	// switch
 	{2, 0, 0, 0, 0, &do_pause, &do_play, 0},
 
@@ -68,10 +69,10 @@ struct gpio_switch switches[] = {
 
 	// middle button
 	{8, 0, 0, 0, 0, 0, &next_album, &player_say_what},
-	
+
 	// right button
 	{9, 0, 0, 0, 0, 0, &next_track, &player_say_what}
-#else
+#	else
 	// right up
 	{9, 0, 0, 0, 0, 0, &next_album, 0},
 
@@ -83,9 +84,9 @@ struct gpio_switch switches[] = {
 
 	// left down
 	{2, 0, 0, 0, 0, 0, &player_pause, &player_say_what}
-#endif // KITCHEN
+#	endif // KITCHEN
 };
-int num_switches = sizeof(switches)/sizeof(struct gpio_switch);
+int num_switches = sizeof( switches ) / sizeof( struct gpio_switch );
 
 pthread_t gpio_input_thread;
 pthread_cond_t gpio_input_changed_cond;
@@ -104,14 +105,14 @@ long last_toggle_time = 0;
 
 void switchIntHandler()
 {
-	for(int i = 0; i < num_switches; i++ ) {
+	for( int i = 0; i < num_switches; i++ ) {
 		switches[i].current_state = digitalRead( switches[i].gpio_pin );
 	}
 	pthread_cond_signal( &gpio_input_changed_cond );
 }
 
-#define NSEC_PER_SEC 1000000000
-void add_ms(struct timespec *ts, int ms)
+#	define NSEC_PER_SEC 1000000000
+void add_ms( struct timespec* ts, int ms )
 {
 	ts->tv_nsec += ms * 1000000;
 
@@ -119,52 +120,53 @@ void add_ms(struct timespec *ts, int ms)
 	ts->tv_nsec = ts->tv_nsec % NSEC_PER_SEC;
 }
 
-void* gpio_input_thread_run( void *p )
+void* gpio_input_thread_run( void* p )
 {
 	//long current_time;
 	int res;
-	Player *player = (Player*) p;
+	Player* player = (Player*)p;
 	pthread_mutex_t mutex;
 	pthread_mutex_init( &mutex, NULL );
 	pthread_mutex_lock( &mutex );
-	LOG_DEBUG("gpio_input_thread_run started");
+	LOG_DEBUG( "gpio_input_thread_run started" );
 
-    struct timespec now;
-    struct timespec wait_time;
-    wait_time.tv_sec = time(NULL);
-    wait_time.tv_nsec = 0;
+	struct timespec now;
+	struct timespec wait_time;
+	wait_time.tv_sec = time( NULL );
+	wait_time.tv_nsec = 0;
 
-	for(;;) {
+	for( ;; ) {
 		res = pthread_cond_timedwait( &gpio_input_changed_cond, &mutex, &wait_time );
-		if( res && res != ETIMEDOUT) {
-			LOG_ERROR("res=d pthread_cond_timedwait returned error", res);
+		if( res && res != ETIMEDOUT ) {
+			LOG_ERROR( "res=d pthread_cond_timedwait returned error", res );
 			return NULL;
 		}
 
-		if( (res = clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &now ) ) ) {
-			LOG_ERROR("res=d clock_gettime failed");
-			assert(0);
+		if( ( res = clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &now ) ) ) {
+			LOG_ERROR( "res=d clock_gettime failed" );
+			assert( 0 );
 		}
 
 		wait_time = now;
 		add_ms( &wait_time, 25 );
 
-		for(int i = 0; i < num_switches; i++ ) {
+		for( int i = 0; i < num_switches; i++ ) {
 			int current_state = switches[i].current_state;
 			if( switches[i].last_state != current_state ) {
-				LOG_INFO("switch=d state=d msg", i, current_state);
+				LOG_INFO( "switch=d state=d msg", i, current_state );
 				switches[i].last_state = current_state;
 				switches[i].changed_at = now.tv_sec;
 				if( !current_state ) {
 					// 0 = switch has been closed (i.e. pressed)
 					if( switches[i].on_down ) {
-						LOG_INFO("switch=d on_down", i);
+						LOG_INFO( "switch=d on_down", i );
 						switches[i].on_down( player );
 					}
-				} else {
+				}
+				else {
 					// 1 = switch is open (i.e. not pressed)
-					if( switches[i].on_up && switches[i].held == 0) {
-						LOG_INFO("switch=d on_up", i);
+					if( switches[i].on_up && switches[i].held == 0 ) {
+						LOG_INFO( "switch=d on_up", i );
 						switches[i].on_up( player );
 					}
 					switches[i].held = 0;
@@ -173,43 +175,43 @@ void* gpio_input_thread_run( void *p )
 			if( !current_state && switches[i].changed_at ) {
 				time_t elapsed_time = now.tv_sec - switches[i].changed_at;
 				//LOG_INFO("switch=d elapsed=d on hold?", i, elapsed_time);
-				if( elapsed_time >= 2 && switches[i].held == 0) {
+				if( elapsed_time >= 2 && switches[i].held == 0 ) {
 					if( switches[i].on_hold ) {
-						LOG_INFO("switch=d on_hold", i);
+						LOG_INFO( "switch=d on_hold", i );
 						switches[i].on_hold( player );
 						switches[i].held = 1; // stop further triggers
 					}
 				}
 			}
-			
 		}
 	}
 	return NULL;
 }
 
-int init_rasp_pi(Player *player) {
+int init_rasp_pi( Player* player )
+{
 	int res;
-	LOG_INFO("initializing raspberry pi settings");
+	LOG_INFO( "initializing raspberry pi settings" );
 
 	pthread_cond_init( &gpio_input_changed_cond, NULL );
 
 	if( wiringPiSetup() == -1 ) {
-		LOG_ERROR("failed to setup wiringPi");
+		LOG_ERROR( "failed to setup wiringPi" );
 		return 1;
 	}
 
-	res = pthread_create( &gpio_input_thread, NULL, &gpio_input_thread_run, (void*) player );
+	res = pthread_create( &gpio_input_thread, NULL, &gpio_input_thread_run, (void*)player );
 	if( res ) {
-		LOG_ERROR("failed to create input thread");
+		LOG_ERROR( "failed to create input thread" );
 		return 1;
 	}
 
-	for(int i = 0; i < num_switches; i++ ) {
+	for( int i = 0; i < num_switches; i++ ) {
 		pinMode( switches[i].gpio_pin, INPUT );
 		pullUpDnControl( switches[i].gpio_pin, PUD_UP );
 		switches[i].last_state = switches[i].current_state = digitalRead( switches[i].gpio_pin );
-		wiringPiISR( switches[i].gpio_pin,  INT_EDGE_BOTH, switchIntHandler);
-		LOG_INFO("switch=d state=d init state", i, switches[i].last_state);
+		wiringPiISR( switches[i].gpio_pin, INT_EDGE_BOTH, switchIntHandler );
+		LOG_INFO( "switch=d state=d init state", i, switches[i].last_state );
 	}
 
 	return 0;
